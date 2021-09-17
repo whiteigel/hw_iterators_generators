4a0952017934a59583ed9e6808ea8b2f969efbab
Sergey Volodin <whiteigel@gmail.com>
2021-09-17 16:31:59 +0200

    Publish

--- main.py
+++ main.py
@@ -12,26 +12,22 @@ class WikiArticle:
         self.start = start
 
     def __iter__(self):
-        self.start = self.start - 1
         return self
 
     def __next__(self):
-        output = []
-        for self.country in self.json:
-            self.country = self.country['name']['common']
-            self.country = self.country.replace(' ', '_')
-            wiki_url = 'https://en.wikipedia.org/wiki/'
-            country_page = wiki_url + self.country
-            country_link_pair = f'{self.country}, {country_page} ' + ' \n'
-            output.append(country_link_pair)
         self.start += 1
-        if self.start >= len(output):
+        if self.start == len(self.json):
             raise StopIteration
-        return output
+        self.country = self.json[self.start]['name']['common']
+        self.country = self.country.replace(' ', '_')
+        wiki_url = 'https://en.wikipedia.org/wiki/'
+        country_page = wiki_url + self.country
+        country_link_pair = f'{self.country}, {country_page} ' + ' \n'
+        return country_link_pair
 
 
 if __name__ == '__main__':
     article = WikiArticle(read_file, 0)
     with open(write_file, 'w') as f:
-        for country in article.__next__():
+        for country in article:
             f.writelines(country)
