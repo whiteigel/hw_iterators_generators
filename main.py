828cf7c5e91e68139e284acf5425b7749a2d90b8
Sergey Volodin <whiteigel@gmail.com>
2021-09-22 10:43:01 +0200

    Code shortening

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import json
 
-
 read_file = 'countries.json'
 write_file = 'countries_with_links.txt'
 
 
 class WikiArticle:
     def __init__(self, path, start):
-        self.file = open(path, encoding='utf-8')
-        self.json = json.load(self.file)
+        self.json = json.load(open(path, encoding='utf-8'))
         self.start = start - 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        wiki_url = 'https://en.wikipedia.org/wiki/'
         self.start += 1
         if self.start == len(self.json):
             raise StopIteration
         self.country = self.json[self.start]['name']['common']
         self.country_link = self.country.replace(' ', '_')
-        wiki_url = 'https://en.wikipedia.org/wiki/'
-        country_page = wiki_url + self.country_link
-        country_link_pair = f'{self.country}, {country_page} ' + ' \n'
-        return country_link_pair
+        return f'{self.country} - {wiki_url}{self.country_link}\n'
 
 
 if __name__ == '__main__':
--- md5.py
+++ md5.py
@@ -3,15 +3,14 @@ import hashlib
 read_file = 'countries_with_links.txt'
 write_file = 'hashes.txt'
 
-res = open(read_file, 'rb')
 
-
-def gen_md5(input):
-    for country in input:
-        yield hashlib.md5(country).hexdigest()
+def gen_md5():
+    with open(read_file, 'rb') as res:
+        for country in res:
+            yield hashlib.md5(country).hexdigest()
 
 
 with open(write_file, 'w') as output:
-    for item in gen_md5(res):
+    for item in gen_md5():
         print(item)
         output.write(item+'\n')
